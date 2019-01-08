c004bad010a3052795373925bad83478a593b8b9
Khoi Ngo <khoint@bsscommerce.com>
2019-01-08 15:09:13 +0000

    fix after merge

--- packages/venia-concept/src/actions/app/asyncActions.js
+++ packages/venia-concept/src/actions/app/asyncActions.js
@@ -10,19 +10,18 @@ export const toggleSearch = () => async dispatch =>
     dispatch(actions.toggleSearch());
 
 export const executeSearch = (query, history, categoryId) =>
-export const toggleCompare = () => async dispatch =>
-    dispatch(actions.toggleCompare());
-
-export const addCompare = product => async dispatch =>
-    dispatch(actions.addCompare(product));
-
-export const executeSearch = (query, history) =>
     async function thunk(dispatch) {
         let searchQuery = `query=${query}`;
         if (categoryId) searchQuery += `&category=${categoryId}`;
         history.push(`/search.html?${searchQuery}`);
         dispatch(actions.executeSearch(query));
     };
+
+export const toggleCompare = () => async dispatch =>
+    dispatch(actions.toggleCompare());
+
+export const addCompare = product => async dispatch =>
+    dispatch(actions.addCompare(product));
 
 export const addItemToCompare = product =>
     async dispatch => {
